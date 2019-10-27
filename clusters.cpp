501f01d45c87dea5c712b6b275833cf697664f32
dhoule <caine2003@gmail.com>
2019-10-27 18:07:24 -0500

    Updates clusters files
    Reorganizes code to make it easier to read. Adds comments.
    
    In clusters.cpp, Clusters::read_file -
      - each node reads a portion of the input file
      - in binary file, size od dataset & # of dimensions has to be 1st 2
    things
      - creates new Points object
        - sets # of dimensions/attributes
        - sets # of points for new object
      - There is a loop with no comments
        it requires output statements to see what is going on…
    - In Clusters::build_kdtree() -
      - There is a `new kdtree2(#,#)` & I can’t find a constructor w/2
    arguments
    - In Clusters::build_kdtree_outer() -
      - Same as above…

--- clusters.cpp
+++ clusters.cpp
@@ -1,260 +1,277 @@
 
 #include "clusters.h"
 
-namespace NWUClustering
-{
-	Clusters::~Clusters()
-	{
-		if(m_pts)
-		{
-			m_pts->m_points.clear();
-			delete m_pts;
-			m_pts = NULL;
-		}
+namespace NWUClustering {
 
-		if(m_kdtree)
-		{
-			delete m_kdtree;
-			m_kdtree = NULL;
-		}
+  // destructor 
+    // can have 4 parameters: m_pts,m_kdtree,m_pts_outer,m_kdtree_outer
+    // Clears, and deletes Points, Points_Outer, & kdtree2 objects
+  Clusters::~Clusters() { // Pure virtual destructor 
+    if(m_pts) {
+      m_pts->m_points.clear();
+      delete m_pts;
+      m_pts = NULL;
+    }
 
-        	if(m_pts_outer)
-        	{
-			m_pts_outer->m_prIDs.clear();
-			m_pts_outer->m_ind.clear();
-            		m_pts_outer->m_points.clear();
-            		delete m_pts_outer;
-            		m_pts_outer = NULL;
-        	}
+    if(m_kdtree) {
+      delete m_kdtree;
+      m_kdtree = NULL;
+    }
 
-	        if(m_kdtree_outer)
-        	{
-	            delete m_kdtree_outer;
-        	    m_kdtree_outer = NULL;
-        	}
-	}
+    if(m_pts_outer) {
+      m_pts_outer->m_prIDs.clear();
+      m_pts_outer->m_ind.clear();
+      m_pts_outer->m_points.clear();
+      delete m_pts_outer;
+      m_pts_outer = NULL;
+    }
 
-	bool Clusters::allocate_outer(int dims)
-	{
-		if(m_pts_outer == NULL)
-        	{
-	            	m_pts_outer = new Points_Outer;
-        		m_pts_outer->m_prIDs.clear();
-            		m_pts_outer->m_ind.clear();
-            		m_pts_outer->m_i_dims = dims;
-            		m_pts_outer->m_i_num_points = 0;
-        	}
-	}
+    if(m_kdtree_outer) {
+      delete m_kdtree_outer;
+      m_kdtree_outer = NULL;
+    }
+  }
 
-	bool Clusters::addPoints(int source, int buf_size, int dims, vector<float>& raw_data)
-	{
-		int i, j, k, pos, num_points = buf_size / dims;
+  // Sets up the Points_Outer objects
+  bool Clusters::allocate_outer(int dims) {
 
-		//cout << "add points called" << endl; 
+    if(m_pts_outer == NULL) {
+      m_pts_outer = new Points_Outer;
+      m_pts_outer->m_prIDs.clear();
+      m_pts_outer->m_ind.clear();
+      m_pts_outer->m_i_dims = dims;
+      m_pts_outer->m_i_num_points = 0;
+    }
+  }
 
-		// incorrect dimension
-		if(m_pts_outer->m_i_dims != dims)
-			return false;
+  // Adds points to a cluster object
+  bool Clusters::addPoints(int source, int buf_size, int dims, vector<float>& raw_data) {
+    // used as itterables
+    int i, j, k; 
+    // pos = original num of points, then used as a key in 'm_pts_outer->m_points'
+    int pos;
+    // num_points = new number of points for the cluster
+    int num_points = buf_size / dims;
+    // TODO This is for tracing
+    //cout << "add points called" << endl; 
 
-		pos = m_pts_outer->m_i_num_points;
-        	m_pts_outer->m_i_num_points += num_points;
-        	//m_pts_outer->m_points.resize(extents[m_pts_outer->m_i_num_points][dims]);
+    // incorrect dimension
+      // the number of dimensions/attributes can never change
+    if(m_pts_outer->m_i_dims != dims)
+      return false;
 
-		//allocate memory for the points
-                m_pts_outer->m_points.resize(m_pts_outer->m_i_num_points);
-                for(int ll = 0; ll < m_pts_outer->m_i_num_points; ll++)
-                	m_pts_outer->m_points[ll].resize(dims);
+    // Save the original number of points, then increase by the new "num_points" value
+    pos = m_pts_outer->m_i_num_points;
+    m_pts_outer->m_i_num_points += num_points;
+    //m_pts_outer->m_points.resize(extents[m_pts_outer->m_i_num_points][dims]);
 
-		m_pts_outer->m_prIDs.resize(m_pts_outer->m_i_num_points, -1);
-		
-		k = 0;
-		for(i = 0; i < num_points; i++)
-		{
-			for(j = 0; j < dims; j++)
-				m_pts_outer->m_points[pos][j] = raw_data[k++];
-			
-			m_pts_outer->m_prIDs[pos] = source;
-			
-			pos++;
-		}
+    //allocate memory for the points
+      // resize() - Resizes the container so that it contains n elements
+    m_pts_outer->m_points.resize(m_pts_outer->m_i_num_points);
+    // resize each new point object
+    for(int ll = 0; ll < m_pts_outer->m_i_num_points; ll++)
+      m_pts_outer->m_points[ll].resize(dims);
+    // resize the point IDs
+    m_pts_outer->m_prIDs.resize(m_pts_outer->m_i_num_points, -1);
 
-		//cout << "outer " << m_pts_outer->m_i_num_points << endl;
+    k = 0;
+    // loop over the new number of points
+    for(i = 0; i < num_points; i++) {
+      // loop over the number of dimensions/attributes
+      for(j = 0; j < dims; j++)
+        // assign the new 'raw_data' elements to the newest points
+        m_pts_outer->m_points[pos][j] = raw_data[k++]; // TODO possible buffer overflow????????
+      // assign the cluster ID for the point
+      m_pts_outer->m_prIDs[pos] = source; // TODO check that source is a cluster ID
+      // increment the counter for the key...
+      pos++;
+    }
 
-		return true;
-	}
-	
-	bool Clusters::updatePoints(vector< vector<int> >& raw_ind)
-	{
-		int i, source = -1, j = -1, prev_source = -1;
-	
-		m_pts_outer->m_ind.resize(m_pts_outer->m_i_num_points, -1);
+    //cout << "outer " << m_pts_outer->m_i_num_points << endl;
 
-		for(i = 0; i < m_pts_outer->m_i_num_points; i++)
-		{
-			source = m_pts_outer->m_prIDs[i];
-			
-			if(source != prev_source)
-				j = 0;
+    return true;
+  }
 
-			m_pts_outer->m_ind[i] = raw_ind[source][j++];
+  // Updates OUTER points' cluster IDs
+  bool Clusters::updatePoints(vector< vector<int> >& raw_ind) {
+    // used as itterables
+    int i, j = -1;
+    // I believe these are cluster IDs
+    int source = -1,  prev_source = -1;
+    // resize 'm_ind', by 'm_i_num_points' spaces, new elements are initialized as copies of -1
+    m_pts_outer->m_ind.resize(m_pts_outer->m_i_num_points, -1);
+    // loop over the Outer points, and update cluster IDs
+    for(i = 0; i < m_pts_outer->m_i_num_points; i++) {
+      source = m_pts_outer->m_prIDs[i];
 
-			prev_source = source;
-		}
-		
-		return true;
-	}
+      if(source != prev_source)
+        j = 0;
 
-	int Clusters::read_file(char* infilename, int isBinaryFile)
-	{
-		ssize_t numBytesRead;
-		int     i, j, rank, nproc;
-		int num_points, dims;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+      m_pts_outer->m_ind[i] = raw_ind[source][j++];
 
-		// only supports binary data file
-		if(isBinaryFile == 1)
-        	{
-			// NOTE: the input data points are equally partioned and each core read their corresponding part
-			// later the points will be partioned geometrically
+      prev_source = source;
+    }
 
-			ifstream file (infilename, ios::in|ios::binary);
-			if(file.is_open())
-  			{
-				file.read((char*)&num_points, sizeof(int));
-				file.read((char*)&dims, sizeof(int));
-    				
-				#ifdef _DEBUG	
-				if(rank == proc_of_interest) cout << "Points " << num_points << " dims " << dims << endl;
-				#endif
+    return true;
+  }
 
-				// compute the respective segments
-			    	long long sch, lower, upper;
+  /*
+    Reads the binary file. 
+    Input data points are equally partioned, each core reading their corresponding part of the file. 
+    Later the points will be partioned geometrically.
+    Points are created here, based off of the data points.
+    According to the README, 'num_points' & 'dims' HAVE to be the 1st 2 things in the file (each 4 bytes).
+  */
+  int Clusters::read_file(char* infilename, int isBinaryFile) {
+    ssize_t numBytesRead; // TODO this isn't even used
+    // used as itterables
+    int i, j;
+    // rank = current node's ID, nproc = total number of nodes in the system
+    int rank, nproc;
+    int num_points, dims;
+    // Get the current node's 'rank'
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // Get the total number of nodes in the system
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-    	    			if(num_points % nproc == 0)
-		       	    		sch = num_points/nproc;
-        			else
-           				sch = num_points/nproc + 1;
+    // only supports binary data file
+    if(isBinaryFile == 1) {
+      // NOTE: the input data points are equally partioned and each core read their corresponding part
+      // later the points will be partioned geometrically
 
-	        		#ifdef _DEBUG
-    	            		if(rank == proc_of_interest)
-        	       		{
-		            	    	cout << "Segment size " << sch << endl;
-                       			cout << "Points per process on average " << num_points/nproc << endl;
-              			}
-        			#endif
+      ifstream file (infilename, ios::in|ios::binary);
+      if(file.is_open()) {
+        // size of the dataset
+        file.read((char*)&num_points, sizeof(int));
+        // number of dimensions for each data point/record
+        file.read((char*)&dims, sizeof(int));
 
-		    	    	lower = sch * rank;
-    		    		upper = sch * (rank + 1);
-	    		    	if(upper > num_points)
-    	    		    		upper = num_points;
+        #ifdef _DEBUG 
+        if(rank == proc_of_interest) cout << "Points " << num_points << " dims " << dims << endl;
+        #endif
 
-				// allocate memory for points
-				m_pts = new Points;
-				m_pts->m_i_dims = dims;
-                		m_pts->m_i_num_points = upper - lower;//num_points;
+        // compute the respective segments of the file to read.
+          // sch = section or search?
+          // lower = the starting position in the file to start reading from
+          // upper = the last position in the file to read from
+        long long sch, lower, upper;
+        // determine the size of the section of file to read
+        if(num_points % nproc == 0)
+          sch = num_points/nproc; // the 2 are evenly divisible
+        else
+          sch = num_points/nproc + 1; // taking care of the remainder, by adding a whole 1
 
-				m_pts->m_box = new interval[m_pts->m_i_dims];	
+        #ifdef _DEBUG
+        if(rank == proc_of_interest) {
+          cout << "Segment size " << sch << endl;
+          cout << "Points per process on average " << num_points/nproc << endl;
+        }
+        #endif
 
-				//allocate memory for the points
-				//m_pts->m_points.resize(extents[num_points][dims]);
-				//m_pts->m_points.resize(extents[m_pts->m_i_num_points][dims]);
-				//allocate memory for the points
-                		m_pts->m_points.resize(m_pts->m_i_num_points);
-                		for(int ll = 0; ll < m_pts->m_i_num_points; ll++)
-                			m_pts->m_points[ll].resize(dims);
+        lower = sch * rank; // the pointer for the starting location
+        upper = sch * (rank + 1); // the pointer for the end location
+        // if the "end location" exceeds the number of points, reduce it to the number of points
+        if(upper > num_points)
+          upper = num_points;
 
-				point_coord_type* pt;					
-				
-        	        	pt = (point_coord_type*) malloc(dims * sizeof(point_coord_type));
-                
-				// fseek to the respective position
-				file.seekg(lower * dims * sizeof(point_coord_type), ios::cur);
+        // allocate memory for points
+        m_pts = new Points;
+        m_pts->m_i_dims = dims; // number of dimensions/attributes
+        m_pts->m_i_num_points = upper - lower; // the number of points
+        // interval struct defined in kdtree2.h
+          // m_box, is of type interval*
+        m_pts->m_box = new interval[m_pts->m_i_dims]; 
 
-        	       		for (i = 0; i < upper - lower; i++)
-				{
-               				file.read((char*)pt, dims*sizeof(point_coord_type));
-                                
-                   			for (j = 0; j < dims; j++)
-					{
-                    				m_pts->m_points[i][j] = pt[j];
-							
-						if(i == 0)
-						{	
-							m_pts->m_box[j].upper = m_pts->m_points[i][j];
-							m_pts->m_box[j].lower = m_pts->m_points[i][j];
-						}
-						else
-						{
-		                   		 	if(m_pts->m_box[j].lower > m_pts->m_points[i][j])
-			                      			m_pts->m_box[j].lower = m_pts->m_points[i][j];
-                    					else if(m_pts->m_box[j].upper < m_pts->m_points[i][j])
-                       						m_pts->m_box[j].upper = m_pts->m_points[i][j];
-						}
-					}
-                		}
-			
-				free(pt);
-				pt = NULL;
+        //allocate memory for the points
+        //m_pts->m_points.resize(extents[num_points][dims]);
+        //m_pts->m_points.resize(extents[m_pts->m_i_num_points][dims]);
+        
 
-				file.close();
-			}	
-			else
-			{
-				cout << "rank " << rank << " Error: no such file: " << infilename << endl;
-				return -1;
-			}
-		}
-		else
-		{
-			cout << "Only supports binary data: Failed to read data" << endl;
-			return -1;
-		}
-		return 0;
-		
-	}
+        //allocate memory for the points
+        m_pts->m_points.resize(m_pts->m_i_num_points);
+        for(int ll = 0; ll < m_pts->m_i_num_points; ll++)
+          m_pts->m_points[ll].resize(dims);
+        // point_coord_type = typedef float point_coord_type; in 'utils.h'
+        point_coord_type* pt;         
+        // initializes 'pt' variable
+        pt = (point_coord_type*) malloc(dims * sizeof(point_coord_type));
 
-	int Clusters::build_kdtree()
-	{
-		if(m_pts == NULL)
-		{
-			cout << "Point set is empty" << endl;
-			return -1;
-		}
+        // fseek to the respective position of the file
+        file.seekg(lower * dims * sizeof(point_coord_type), ios::cur);
+        // loop over the area of the file for the node
+          // TODO possible speed up: put 'upper - lower' into a local variable...
+        for (i = 0; i < upper - lower; i++) {
+          // signature: istream& read (char* s, streamsize n);
+          // Extracts n characters from the stream and stores them in the array pointed to by s.
+          file.read((char*)pt, dims * sizeof(point_coord_type));
 
-		//m_kdtree = new kdtree2(m_pts->m_points, true);
-		m_kdtree = new kdtree2(m_pts->m_points, false);
-		
-		if(m_kdtree == NULL)
-		{
-			cout << "Falied to allocate new kd tree for orginial points" << endl;
-			return -1;
-		}
-		
-		return 0;		
-	} 
+          for (j = 0; j < dims; j++) {
+            m_pts->m_points[i][j] = pt[j];
+            // TODO This will take some output to determine what is going on...
+            if(i == 0) { 
+              m_pts->m_box[j].upper = m_pts->m_points[i][j];
+              m_pts->m_box[j].lower = m_pts->m_points[i][j];
+            } else {
+              if(m_pts->m_box[j].lower > m_pts->m_points[i][j])
+                m_pts->m_box[j].lower = m_pts->m_points[i][j];
+              else if(m_pts->m_box[j].upper < m_pts->m_points[i][j])
+                m_pts->m_box[j].upper = m_pts->m_points[i][j];
+            }
+          }
+        }
 
-	int Clusters::build_kdtree_outer()
-	{
-		if(m_pts_outer == NULL)
-		{
-			cout << "Outer point set is empty" << endl;
-			return -1;
-		}
-		
-		if(m_pts_outer->m_i_num_points > 0)
-		{
+        free(pt);
+        pt = NULL;
 
-			//m_kdtree_outer = new kdtree2(m_pts->m_points_outer, true);
-			m_kdtree_outer = new kdtree2(m_pts_outer->m_points, false);
-		
-			if(m_kdtree_outer == NULL)
-			{
-				cout << "Falied to allocate new kd tree for outer points" << endl;
-				return -1;
-			}
-		}
+        file.close();
+      } else {
+        cout << "rank " << rank << " Error: no such file: " << infilename << endl;
+        return -1;
+      }
+    } else {
+      cout << "Only supports binary data: Failed to read data" << endl;
+      return -1;
+    }
+    return 0;
+  }
+  // Called from mpi_main...
+  int Clusters::build_kdtree() {
+    // if the Point objects weren't created, don't bother going further...
+    if(m_pts == NULL) {
+      cout << "Point set is empty" << endl;
+      return -1;
+    }
 
-		return 0;		
-	} 
+    //m_kdtree = new kdtree2(m_pts->m_points, true);
+    // TODO I can't find this signiture!!!!
+    m_kdtree = new kdtree2(m_pts->m_points, false);
+
+    if(m_kdtree == NULL) {
+      cout << "Falied to allocate new kd tree for orginial points" << endl;
+      return -1;
+    }
+
+    return 0;   
+  } 
+  // Called from mpi_main...
+  int Clusters::build_kdtree_outer() {
+
+    if(m_pts_outer == NULL) {
+      cout << "Outer point set is empty" << endl;
+      return -1;
+    }
+
+    if(m_pts_outer->m_i_num_points > 0) {
+
+      //m_kdtree_outer = new kdtree2(m_pts->m_points_outer, true);
+      m_kdtree_outer = new kdtree2(m_pts_outer->m_points, false);
+
+      if(m_kdtree_outer == NULL) {
+        cout << "Falied to allocate new kd tree for outer points" << endl;
+        return -1;
+      }
+    }
+
+    return 0;   
+  } 
 }
