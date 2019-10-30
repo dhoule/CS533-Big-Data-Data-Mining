7c9d96c3fa84c79f35f574a775b23a0112796bd7
dhoule <caine2003@gmail.com>
2019-10-30 10:30:35 -0500

    Updates utils.cpp
    Reorders the code and changes \t to \s

--- utils.cpp
+++ utils.cpp
@@ -1,10 +1,10 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /*   Files: mpi_main.cpp clusters.cpp  clusters.h utils.h utils.cpp          */
-/*   			dbscan.cpp dbscan.h kdtree2.cpp kdtree2.hpp          */
-/*			geometric_partitioning.h geometric_partitioning.cpp  */
-/*		    						             */
+/*        dbscan.cpp dbscan.h kdtree2.cpp kdtree2.hpp          */
+/*      geometric_partitioning.h geometric_partitioning.cpp  */
+/*                                 */
 /*   Description: an mpi implementation of dbscan clustering algorithm       */
-/*				using the disjoint set data structure        */
+/*        using the disjoint set data structure        */
 /*                                                                           */
 /*   Author:  Md. Mostofa Ali Patwary                                        */
 /*            EECS Department, Northwestern University                       */
@@ -13,44 +13,41 @@
 /*   Copyright, 2012, Northwestern University                                */
 /*   See COPYRIGHT notice in top-level directory.                            */
 /*                                                                           */
-/*   Please cite the following publication if you use this package 	     */
-/* 									     */
+/*   Please cite the following publication if you use this package       */
+/*                       */
 /*   Md. Mostofa Ali Patwary, Diana Palsetia, Ankit Agrawal, Wei-keng Liao,  */
 /*   Fredrik Manne, and Alok Choudhary, "A New Scalable Parallel DBSCAN      */
 /*   Algorithm Using the Disjoint Set Data Structure", Proceedings of the    */
 /*   International Conference on High Performance Computing, Networking,     */
-/*   Storage and Analysis (Supercomputing, SC'12), pp.62:1-62:11, 2012.	     */
+/*   Storage and Analysis (Supercomputing, SC'12), pp.62:1-62:11, 2012.      */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "utils.h"
 
 // Find Kth element without recusion
-float findKMedian(vector<float>& A,int K)
-{
-	int l,m;
-	l=0;
-	m=A.size()-1;
-	while (l<m) 
-	{
-		float x=A[K];
-		int i=l;
-		int j=m;
-		do {
-			while (A[i]<x) i++;
-			while (x<A[j]) j--;
-			if (i<=j) 
-			{
-				swap(A[i], A[j]);
-				i++; 
-				j--;
-			}
-		} while (i<=j);
+float findKMedian(vector<float>& A,int K) {
+  int l,m;
+  l=0;
+  m=A.size()-1;
+  while (l<m) {
+    float x=A[K];
+    int i=l;
+    int j=m;
+    do {
+      while (A[i]<x) i++;
+        while (x<A[j]) j--;
+          if (i<=j) {
+            swap(A[i], A[j]);
+            i++; 
+            j--;
+          }
+    } while (i<=j);
 
-		if (j<K) l=i;
-		if (K<i) m=j;
-	}
+    if (j<K) l=i;
+    if (K<i) m=j;
+  }
 
-	return A[K];
+  return A[K];
 }
 
 
