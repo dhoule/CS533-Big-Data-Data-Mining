efe76533d4aacbda2dec3ff24ecd9935528ff430
dhoule <caine2003@gmail.com>
2019-11-06 10:58:00 -0600

    Updates clusters.h
    #1 Adds comments to help describe the variables purpose.

--- clusters.h
+++ clusters.h
@@ -7,20 +7,20 @@
 
 namespace NWUClustering {
   struct Points {
-    array2dfloat m_points;
-    int m_i_dims;
-    int m_i_num_points;
-    interval* m_box;  
+    array2dfloat m_points; // actual point values
+    int m_i_dims; // number of dimensions
+    int m_i_num_points; // number of points in `m_points` attribute
+    interval* m_box; // struct, containing attribute `lower` & `upper`.
   };
 
   struct Points_Outer {
-    array2dfloat m_points;
-    vector <int> m_prIDs;
-    vector <int> m_ind;
+    array2dfloat m_points; // actual point values
+    vector <int> m_prIDs; // pointer IDs for the cluster
+    vector <int> m_ind; // has something to do with cluster IDs for points. TODO
 
-    int m_i_dims;
-    int m_i_num_points;
-    interval* m_box;
+    int m_i_dims; // number of dimensions
+    int m_i_num_points; // number of points in `m_points` attribute
+    interval* m_box; // struct, containing attribute `lower` & `upper`.
   };
 
   class Clusters {
