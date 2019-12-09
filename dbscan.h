d53c8d2792d8127bea532e4f8f8023fb9c09d5da
dhoule <caine2003@gmail.com>
2019-12-09 15:13:04 -0600

    Updates dbscan.h
    #13 #12 Removes unused variable `m_messages_per_round`. Adds comments
    for what the variables are.

--- dbscan.h
+++ dbscan.h
@@ -44,17 +44,16 @@ namespace NWUClustering {
 
   public:
     
-    double  m_epsSquare;
-    int   m_minPts;
-    int   m_messages_per_round;
+    double  m_epsSquare; // AKA radius. It is the square of the "radius" given by the user
+    int   m_minPts; // The minimum number of points, given by the user, to start a cluster
     int   m_compression;
 
     vector <int> m_parents; // Elements hold the pointers of the clustering tree
     vector <int> m_parents_pr; // Elements hold the pointers for which node the point is in
-    vector <int> m_child_count; 
+    vector <int> m_child_count; // number of "children" a possible centroid has
 
-    vector <int> m_member;
-    vector <int> m_corepoint;
+    vector <int> m_member; // Values are either 0 or 1. It's size = size_of(m_pts.m_i_num_points). Used to determine if a border point or not.
+    vector <int> m_corepoint; // Values are either 0 or 1. It's size = size_of(m_pts.m_i_num_points). Used to determine centroids.
   };  
 
   void run_dbscan_algo_uf_mpi_interleaved(ClusteringAlgo& dbs); // union find dbscan algorithm using mpi with interleaved communication
