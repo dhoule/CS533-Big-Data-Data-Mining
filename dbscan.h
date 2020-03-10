27d3dd0b2402982a8f40a13e8a3b1d64cfd98643
dhoule <caine2003@gmail.com>
2020-03-10 13:18:46 -0500

    Updates dbscan.h
    #45 Adds signature for function unionize_neighborhood()

--- dbscan.h
+++ dbscan.h
@@ -63,6 +63,7 @@ namespace NWUClustering {
 
   void run_dbscan_algo_uf_mpi_interleaved(ClusteringAlgo& dbs); // union find dbscan algorithm using mpi with interleaved communication
   void get_neighborhood_points(ClusteringAlgo& dbs, kdtree2_result_vector &ne, kdtree2_result_vector &ne_outer, int pid);
+  void unionize_neighborhood(ClusteringAlgo& dbs, kdtree2_result_vector &ne, kdtree2_result_vector &ne_outer, int pid, vector < vector <int > >* p_cur_insert);
 };
 
 #endif
